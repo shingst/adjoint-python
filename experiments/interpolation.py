7b4548b6ebeb046ee2b8c4127675c8c5488b1963
Sven Hingst <sven.hingst@tum.de>
2022-01-14 14:15:07 +0100

    first version of adjoint over time

--- experiments/interpolation.py
+++ experiments/interpolation.py
@@ -3,6 +3,7 @@ import numpy as np
 from vtkmodules.util import numpy_support
 import os
 from pathlib import Path
+from string import Template
 
 def readUnstructuredGrid(filename) -> vtk.vtkUnstructuredGrid:
 	reader=vtk.vtkUnstructuredGridReader()
@@ -12,13 +13,36 @@ def readUnstructuredGrid(filename) -> vtk.vtkUnstructuredGrid:
 	reader.Update()
 	return reader.GetOutput()
 
-def plot_numpy_array(arr, ugrid):
+def writeUnstructuredGrid(filename,ugrid):
+	writer:vtk.vtkUnstructuredGridWriter =vtk.vtkUnstructuredGridWriter()
+	writer.SetFileName(filename)
+	writer.SetInputData(ugrid)
+	# writer.SetInputConnection(0,interp.GetOutputPort(0))
+	writer.Write()
+
+def write_numpy_array(arr, ugrid_to_copy,filename):
 	m_max=arr.max()
 	m_min=arr.min()
 
+	ugrid: vtk.vtkUnstructuredGrid=vtk.vtkUnstructuredGrid()
+	ugrid.DeepCopy(ugrid_to_copy)
+
 	magdata: vtk.vtkArray=numpy_support.numpy_to_vtk(arr)
 	magdata.SetName("Magnitude")
-	#! this reuses the previous pointset
+	# pointsset.GetPointData().AddArray(magdata) #TODO set name
+	ugrid.GetPointData().SetActiveScalars('Magnitude')
+	ugrid.GetPointData().SetScalars(magdata)
+	writeUnstructuredGrid(filename,ugrid)
+
+def plot_numpy_array(arr, ugrid_to_copy):
+	m_max=arr.max()
+	m_min=arr.min()
+	
+	ugrid:vtk.vtkUnstructuredGrid=vtk.vtkUnstructuredGrid()
+	ugrid.DeepCopy(ugrid_to_copy)
+
+	magdata: vtk.vtkArray=numpy_support.numpy_to_vtk(arr)
+	magdata.SetName("Magnitude")
 	# pointsset.GetPointData().AddArray(magdata) #TODO set name
 	ugrid.GetPointData().SetActiveScalars('Magnitude')
 	ugrid.GetPointData().SetScalars(magdata)
@@ -90,22 +114,87 @@ def handle_two_files(forward_file,adjoint_file):
 	onlypoints.SetCells(data.GetCellTypesArray(), data.GetCells())#needed for visualization
 	return impact,onlypoints
 
+def adjoint_over_time(forward_template:Template,adjoint_template:Template,start, finish):
+	"""
+	:param forward_template: 
+	:param adjoint_template: 
+	:param start: 
+	:param finish: larger than the last index
+	:return: 
+	"""
+	data=readUnstructuredGrid(forward_template.substitute({'file':'1'}))
+	pts: vtk.vtkPoints=data.GetPoints()
+
+	onlypoints: vtk.vtkUnstructuredGrid=vtk.vtkUnstructuredGrid()
+	onlypoints.SetPoints(pts)
+	num_pts=pts.GetNumberOfPoints()
+	
+	adjoints=np.zeros((finish-start,num_pts,5))
+	for i in range(finish-start):
+		adj:vtk.vtkUnstructuredGrid=readUnstructuredGrid(adjoint_template.substitute({'file': i+start}))
+		adj_interpolated: vtk.vtkUnstructuredGrid=interpolate(onlypoints, adj)  #TODO use pipelines
+		pointdata: vtk.vtkPointData=adj_interpolated.GetPointData()
+		vtkQ=pointdata.GetArray('Q')
+		aQ=numpy_support.vtk_to_numpy(vtkQ)
+		adjoints[i,:,:]=aQ
+	return adjoints
+
+
+
+
+
+
+
+
+
+
 # fws=list((Path("/home/sven/exa/adjoint/forward/output/")).glob("pointsource-*.vtk"))
 # fws=os.listdir("/home/sven/exa/adjoint/forward/output/")
-numfiles=35
+numfiles=89
 #TODO better file reading
-forward_file="/home/sven/exa/adjoint/forward/output/pointsource-10.vtk"
-adjoint_file="/home/sven/exa/adjoint/adjoint/outputA/adaptive2a-20.vtk"
+# forward_file="/home/sven/exa/adjoint/forward/output/pointsource-10.vtk"
+# adjoint_file="/home/sven/exa/adjoint/adjoint/outputA/adaptive2a-20.vtk"
 
-if True:
+if False:
 	for i in range(1,numfiles):
 		forward_file=f"/home/sven/exa/adjoint/forward/output/pointsource-{i}.vtk"
-		adjoint_file_file=f"/home/sven/exa/adjoint/adjoint/outputA/constsource-{numfiles-i}.vtk"
+		adjoint_file=f"/home/sven/exa/adjoint/adjoint/outputA/constsource-{numfiles-i}.vtk"
 		impact, onlypoints=handle_two_files(forward_file, adjoint_file)
 		i_normalized=impact/impact.max()
 		if i==1:
 			refine=np.zeros(impact.size)
+		# refine=np.logical_or(refine, i_normalized>0.9)
+		refine+=(i_normalized>0.9).astype(int)
+		
+		
+forward_file=Template("/home/sven/exa/adjoint/forward/output/pointsource-$file.vtk")
+adjoint_file=Template("/home/sven/exa/adjoint/adjoint/outputA/constsource-$file.vtk")
+
+adjoints=adjoint_over_time(forward_file,adjoint_file,1,90)
+for i in range(1,89):
+	data=readUnstructuredGrid(forward_file.substitute({'file': i}))
+	pointdata: vtk.vtkPointData=data.GetPointData()
+	vtkQ=pointdata.GetArray('Q')
+	fQ=numpy_support.vtk_to_numpy(vtkQ)
+
+	onlypoints: vtk.vtkUnstructuredGrid=vtk.vtkUnstructuredGrid()
+	onlypoints.SetPoints(data.GetPoints())
+	for j in range(89-i):
+		magnitude=np.abs(np.sum(fQ*adjoints[j,:,:], axis=1))  #scalar product for each point)
+		impact=np.log10(magnitude+1e-9)
+		i_normalized=impact/impact.max()
+		if j==0:
+			refine=np.zeros(impact.size)
+		# refine+=(i_normalized>0.9).astype(int)
 		refine=np.logical_or(refine, i_normalized>0.9)
+		aa=1
+	onlypoints.SetCells(data.GetCellTypesArray(), data.GetCells())
+	write_numpy_array(refine.astype(int),onlypoints,f"outputE/version1-{i}.vtk")
+	if i==3:break
+	# plot_numpy_array(refine.astype(int), onlypoints)
+		
+		
+
 		
 
 # result.SetCells(data.GetCellTypesArray(),data.GetCells())
@@ -134,8 +223,4 @@ plot_numpy_array(refine.astype(int),onlypoints)#! warning alters onlypoints
 
 
 a=0
-# writer:vtk.vtkUnstructuredGridWriter =vtk.vtkUnstructuredGridWriter()
-# writer.SetFileName("magnitude.vtk")
-# writer.SetInputData(onlypoints)
-# # writer.SetInputConnection(0,interp.GetOutputPort(0))
-# writer.Write()
+
