e84fca1134ff3589bf3075a13d8521b6917b7b1c
Sven Hingst <sven.hingst@tum.de>
2022-04-12 16:32:25 +0200

    improved comparison file

--- experiments/parseexahype.py
+++ experiments/parseexahype.py
@@ -9,15 +9,34 @@ import matplotlib.pyplot as plt
 # pr=pd.read_csv("/home/sven/x/steamdisk/thesisdata/secondcomp/secondfull.probe")
 # fine=pr.values
 
-pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/hel/coarse5.probe")
+pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/f10thirds10.probe")
 coarse=pr.values
-pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/hel/a5thirds5.probe")
+if coarse.shape[0]==151:
+	coarse=np.delete(coarse, 100, 0)  #is plotted twice
+
+pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/f10quarter10.probe")
 staticadj=pr.values
 staticadj=np.delete(staticadj,100,0)#is plotted twice
-pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/hel/full5-rank-9.probe")
+pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/full10-rank-26.probe")
 fine=pr.values
 
+def RMS(approx,fgrid,col):
+	a=approx[:, col]-fgrid[:,col]
+	return np.sqrt((a@a)/(fgrid[:, col]@fgrid[:, col]))
+
+
 Qnames=['σ_11','σ_22','σ_12','u','v']
+
+def timeerror(probe):
+	terr=probe[1:,0]-probe[:-1,0]
+	avg=np.average(np.abs(terr))
+
+
+
+
+for i in range(5):
+	print(f"{Qnames[i]}:RMS of first :{RMS(coarse,fine,i+2)}  RMS second:{RMS(staticadj,fine,i+2)}")
+
 
 nh=np.round(coarse[:,2:7]-fine[:,2:7],8)
 adj=np.round(staticadj[:,2:7]-fine[:,2:7],8)
@@ -30,11 +49,11 @@ nhrel=np.abs(nh/roundfine)
 adjrel=np.abs(adj/roundfine)
 
 for i in range(5):
-	plt.plot(coarse[:, 0], nhrel[:,i], label='no heart')
+	plt.plot(coarse[:, 0], nhrel[:,i], label='coarse')
 	plt.plot(coarse[:, 0], adjrel[:,i], label='adjoined refined')
 	plt.semilogy()
 	plt.legend()
-	plt.title(f"5relative errors of {Qnames[i]}")
+	plt.title(f"3relative errors of {Qnames[i]}")
 	plt.show()
 	
 	
@@ -47,8 +66,8 @@ adjrel[np.isinf(adjrel)]=0.0
 nhsqt=0.0
 adjsqt=0.0
 for i in range(5):
-	nhsq=nhrel[22:,i]@nhrel[22:,i]/nhrel.shape[0]
-	adjsq=nhrel[22:,i]@adjrel[22:,i]/adjrel.shape[0]
+	nhsq=np.sqrt(nhrel[22:,i]@nhrel[22:,i]/nhrel.shape[0])
+	adjsq=np.sqrt(nhrel[22:,i]@adjrel[22:,i]/adjrel.shape[0])
 	print(f"mean squared error of {Qnames[i]} refined:{adjsq} without the heart shape:{nhsq}")
 	nhsqt+=nhsq
 	adjsqt+=adjsq
