a85e129a44261edc74fb6e66a2b892fa69dbe8cf
Sven Hingst <sven.hingst@tum.de>
2022-05-28 11:25:23 +0200

    minor improvements to check +compare probes

--- experiments/checkprobes.py
+++ experiments/checkprobes.py
@@ -12,13 +12,17 @@ import scipy.interpolate as sci
 # file1cs=''
 # file2adj=''
 
-# file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/manual10thirds10.probe"
-file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/amr8t8.probe"
+# file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/pwaves/inner12a12.probe"
+# file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/pwaves/manual12a12.probe"
+file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/wel/inner11D11.probe"
+file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/wel/inner11stressD11.probe"
+# file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/amr8t8.probe"
 # file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/manual10thirds10.probe"
-file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/f8thirds8.probe"
-# file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/tune10f10.probe"
-# file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/tune10e10.probe"
-
+# file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/f8thirds8.probe"
+# file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/tunenoamr10t10.probe"
+# file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/tunenoamr10v10.probe"
+# file1cs= "/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/inner18stress18.probe"
+# file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/manual18a18.probe"
 
 
 pr=pd.read_csv(file1cs)
@@ -28,13 +32,26 @@ if coarse.shape[0]==151:
 
 pr=pd.read_csv(file2adj)
 staticadj=pr.values
+if staticadj.shape[0]==151:
+	staticadj=np.delete(staticadj, 100, 0)  #is plotted twice
+
 # staticadj=np.delete(staticadj,100,0)#is plotted twice
-pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/full8.probe")
+# pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/full10.probe")
+pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/wel/full11.probe")
+# pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/pwaves/full12.probe")
+# pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/fine18.probe")
 fine=pr.values
+if fine.shape[0]==151:
+	fine=np.delete(fine, 100, 0)  #is plotted twice
+
 
 def RMS(approx,fgrid,col):
 	a=approx[:, col]-fgrid[:,col]
 	return np.sqrt((a@a)/(fgrid[:, col]@fgrid[:, col]))
+
+def RMSall(approx, fgrid):
+	a=(approx[:, 2:7]-fgrid[:, 2:7])
+	return np.sqrt(np.sum(a*a)/np.sum(fgrid[:, 2:7]*(fgrid[:, 2:7])))
 
 
 Qnames=['σ_11','σ_22','σ_12','u','v']
@@ -43,7 +60,7 @@ def timeerror(probe):
 	terr=probe[1:,0]-probe[:-1,0]
 	avg=np.average(np.abs(terr))
 
-def resampleprobe(probe,interval=0.02,start=0.0,end=3.0):
+def resampleprobe(probe,interval=0.02,start=0.0,end=2.98):
 	time_points=np.arange(start, end+interval, interval)
 	ret=np.zeros_like(probe)
 	ret[:,0]=time_points
@@ -53,15 +70,16 @@ def resampleprobe(probe,interval=0.02,start=0.0,end=3.0):
 		ret[:,i+2]=int1(time_points)
 	return ret
 
-coarse=resampleprobe(coarse[:128,:],end=2.54)
-fine=resampleprobe(fine[:128,:],end=2.54)
-staticadj=resampleprobe(staticadj[:128,:],end=2.54)
+coarse=resampleprobe(coarse)
+fine=resampleprobe(fine)
+staticadj=resampleprobe(staticadj)
 
 
 
 for i in range(5):
 	print(f"{Qnames[i]}:RMS of first :{RMS(coarse,fine,i+2)}  RMS second:{RMS(staticadj,fine,i+2)}")
 
+print(f"overall RMS of first :{RMSall(coarse,fine)}  RMS second:{RMSall(staticadj,fine)}")
 
 cse=np.round(coarse[:,2:7]-fine[:,2:7],8)
 adj=np.round(staticadj[:,2:7]-fine[:,2:7],8)
@@ -74,8 +92,8 @@ coarserel=np.abs(cse/roundfine)
 adjrel=np.abs(adj/roundfine)
 
 for i in range(5):
-	plt.plot(coarse[:, 0], coarserel[:,i], label='coarse')
-	plt.plot(coarse[:, 0], adjrel[:,i], label='adjoined refined')
+	plt.plot(coarse[:, 0], coarserel[:,i], label='first')
+	plt.plot(coarse[:, 0], adjrel[:,i], label='second')
 	plt.semilogy()
 	plt.legend()
 	plt.title(f"3relative errors of {Qnames[i]}")
--- experiments/compareprobes.py
+++ experiments/compareprobes.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as  np
 import matplotlib.pyplot as plt
 import scipy.interpolate as sci
-
+import matplotlib
+import sys
 
 # pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/second.probe")
 # # pr=pd.read_csv("/home/sven/uni/mt/data/firstexamplerefinementcompare/coarse.probe")
@@ -11,14 +12,22 @@ import scipy.interpolate as sci
 # staticref=pr.values
 # pr=pd.read_csv("/home/sven/uni/mt/data/firstexamplerefinementcompare/fine.probe")
 # fine=pr.values
+# file1cs="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/pwaves/inner12a12.probe"
+# file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/pwaves/manual12a12.probe"
+# file1cs= "/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/inner13a13.probe"
+# file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/inner13B13.probe"
+file1cs= "/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/inner18a18.probe"
+file2adj="/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/manual18a18.probe"
 
 # pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/sismo/rec1c.probe")
-pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/coarse8.probe")
+pr=pd.read_csv(file1cs)
 coarse=pr.values
-pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/f8thirds8.probe")
+pr=pd.read_csv(file2adj)
 staticref=pr.values
 # staticref=np.delete(staticref,100,0)#is plotted twice
-pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/full8-rank-26.probe")
+# pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/output/bel/full10.probe")
+# pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/pwaves/full12.probe")
+pr=pd.read_csv("/home/sven/uni/mt/ExaHyPE-Engine/adjoint/forward/probes/simple/fine18.probe")
 fine=pr.values
 
 
@@ -42,30 +51,34 @@ fine=pr.values
 # plt.plot(coarse[:, 0], statnorm, label='σ_11 static')
 # int1=sci.interp1d(coarse[:,0],coarse[:,2])
 # cc=int1()
-
-def resampleprobe(probe,interval=0.02,start=0.0,end=3.0):
-	time_points=np.arange(0, 3, 0.02)
-	ret=np.zeros_like(probe)
-	ret[:,0]=time_points
-	ret[:,1]=time_points
-	for i in range(5):
-		int1=sci.interp1d(probe[:,0],probe[:,i+2])
-		ret[:,i+2]=int1(time_points)
-	return ret
-
-cc=resampleprobe(coarse)
+# 
+# def resampleprobe(probe,interval=0.02,start=0.0,end=3.0):
+# 	time_points=np.arange(0, 3, 0.02)
+# 	ret=np.zeros_like(probe)
+# 	ret[:,0]=time_points
+# 	ret[:,1]=time_points
+# 	for i in range(5):
+# 		int1=sci.interp1d(probe[:,0],probe[:,i+2])
+# 		ret[:,i+2]=int1(time_points)
+# 	return ret
+# 
+# cc=resampleprobe(coarse)
 
 
 # coarse=fine
-# col=2
+col=2
+if len(sys.argv)==2:
+	col=int(sys.argv[1])
+
 # # plt.plot(coarse[:, 0], (coarse[:, col]-staticref[:150,col])/(staticref[:150,col]+1e-9), label='σ_11 cmp')
-# # plt.plot(staticref[:, 0], staticref[:, col], label='σ_11 static')
-# # plt.plot(fine[:, 0], fine[:, col], label='σ_11 fine')
-# plt.plot(coarse[:, 0], coarse[:, col], label='σ_11 coarse')
+# matplotlib.use('Gtk3Cairo')
+plt.plot(staticref[:, 0], staticref[:, col], label='σ_11 manual')
+plt.plot(fine[:, 0], fine[:, col], label='σ_11 fine')
+plt.plot(coarse[:, 0], coarse[:, col], label='σ_11 Adjrefined')
 # plt.plot(coarse[:, 0], coarse[:, 3], label='σ_22')
 # plt.plot(coarse[:, 0], coarse[:, 4], label='σ_12')
 # # plt.plot(coarse[:, 0], coarse[:, 5], label='u')
 # # plt.plot(coarse[:, 0], coarse[:, 6], label='v')
 # # plt.semilogy()
-# plt.legend()
-# plt.show()
+plt.legend()
+plt.show()
